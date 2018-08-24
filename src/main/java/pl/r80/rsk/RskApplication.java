68ad1c3df0625cbf0f6493ac00c0903821bb2f60
Jarosław Waszczuk <jarekwaszczuk@poczta.fmp>
2018-08-24 17:52:31 +0200

    Podstawowe klasy java

---
+++ src/main/java/pl/r80/rsk/RskApplication.java
@@ -0,0 +1,12 @@
+package pl.r80.rsk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RskApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/r80/rsk/RskApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.r80.rsk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RskApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
