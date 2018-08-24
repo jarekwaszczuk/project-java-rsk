e2cc812ca60453728e7e73df7819e2488d0ed5d9
Jarosław Waszczuk <jarekwaszczuk@poczta.fmp>
2018-08-25 00:29:19 +0200

    dodanie do widoku kadrykartoteki akcji (podgląd, edycja, usuń)

--- src/main/java/pl/r80/rsk/EmployeeRepository.java
+++ src/main/java/pl/r80/rsk/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package pl.r80.rsk;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
 
 }
---
+++ src/main/java/pl/r80/rsk/EmployeeService.java
@@ -0,0 +1,19 @@
+package pl.r80.rsk;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAll() {
+        return (List<Employee>) employeeRepository.findAll();
+    }
+}
